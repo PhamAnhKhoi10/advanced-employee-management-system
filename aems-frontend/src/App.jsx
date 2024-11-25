141bfff53704087340dd8f47f75604d112dcb08a
PhamAnhKhoi10 <ITCSIU23018@student.hcmiu.edu.vn>
2024-11-25 23:54:58 +0700

    Khoi | create landing page

--- aems-frontend/src/App.jsx
+++ aems-frontend/src/App.jsx
@@ -5,16 +5,18 @@ import Dashboard from "./page/Dashboard";
 import Login from "./page/Login";
 import Notification from "./page/Notification";
 import PrivateRoute from "./components/PrivateRoute";
+import Landing from "./page/Landing";
 
 const App = () => {
   return (
     <Routes>
       {/* Put all routes that does not need Nav bar and Side Bar Here */}
       <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Landing/>}/>
 
       {/* Put all routes that need Nav bar and Side Bar Here */}
       <Route element={<Layout />}>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/notifications" element={<Notification />} />
       </Route>
     </Routes>
